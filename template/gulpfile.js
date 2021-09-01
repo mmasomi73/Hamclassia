fb4353184ba38aa25b2318aec416478d1ec49fa4
mmasomi <mmasomi73@gmail.com>
2021-09-01 14:04:18 +0430

    init Template 🐛
    V 0.0.4

--- template/gulpfile.js
+++ template/gulpfile.js
@@ -1,36 +1,47 @@
-var plugins = require("gulp-load-plugins");
-var gulp = require("gulp");
-var sass = require("gulp-sass");
-var sourcemaps = require("gulp-sourcemaps");
-var autoprefixer = require("autoprefixer");
+const plugins = require("gulp-load-plugins");
+const gulp = require("gulp");
+const sass = require('gulp-sass')(require('node-sass'));
+const sourcemaps = require("gulp-sourcemaps");
+const autoprefixer = require("autoprefixer");
+const cleanCSS = require('gulp-clean-css');
+
+// sass.compiler = require('node-sass');
 
 // Load all Gulp plugins into one variable
 const $ = plugins();
 
 gulp.task("Sass", function () {
-  const tailwindcss = require("tailwindcss");
-  const postCssPlugins = [
-    tailwindcss("./tailwind.config.js"),
-    // Autoprefixer
-    autoprefixer(),
+    const tailwindcss = require("tailwindcss");
+    const postCssPlugins = [
+        tailwindcss("./tailwind.config.js"),
+        // Autoprefixer
+        autoprefixer(),
 
-    // UnCSS - Uncomment to remove unused styles in production
-    // PRODUCTION && uncss.postcssPlugin(UNCSS_OPTIONS),
-  ].filter(Boolean);
+        // UnCSS - Uncomment to remove unused styles in production
+        // PRODUCTION && uncss.postcssPlugin(UNCSS_OPTIONS),
+    ].filter(Boolean);
 
-  return gulp
-    .src("src/sass/app.scss")
-    .pipe(sourcemaps.init())
-    .pipe(
-      sass({
-        includePaths: "src/**/*",
-      }).on("error", sass.logError)
-    )
-    .pipe($.postcss(postCssPlugins))
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest("src/"));
+    return gulp
+        .src("src/sass/app.scss")
+        .pipe(sourcemaps.init())
+        .pipe(
+            sass({
+                includePaths: "src/**/*",
+                outputStyle: 'compressed',
+                style: 'compressed'
+
+            }).on("error", sass.logError)
+        )
+        .pipe($.postcss(postCssPlugins))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest("src/css/"));
 });
 
 gulp.task("watch-sass", function () {
-  gulp.watch("src/sass/**/*.scss", gulp.series("Sass"));
+    gulp.watch("src/sass/**/*.scss", gulp.series("Sass"));
+});
+gulp.task('minify-css', () => {
+    return gulp.src('src/css/*.css')
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(gulp.dest('src/css/'));
 });
